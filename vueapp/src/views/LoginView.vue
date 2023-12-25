71e342bfea380d224a37bee71367b42e28679709
Dggrdg <kobe640670@gmail.com>
2023-12-25 10:39:56 +0800

    20231225

--- vueapp/src/views/LoginView.vue
+++ vueapp/src/views/LoginView.vue
@@ -5,11 +5,13 @@
                 <form class="row g-3 needs-validation" novalidate>
                     <div class="col-md-12">
                         <label for="validationCustom01" class="form-label">帳號</label>
-                        <input type="text" class="form-control" id="validationCustom01" v-model="accountValue" required>
+                        <input type="text" class="form-control" id="validationCustom01" v-model.trim="userData.name"
+                            required>
                     </div>
                     <div class="col-md-12">
                         <label for="validationCustom02" class="form-label">密碼</label>
-                        <input type="text" class="form-control" id="validationCustom02" v-model="passwordValue" required>
+                        <input type="password" class="form-control" id="validationCustom02" v-model.trim="userData.password"
+                            required>
                     </div>
                     <div class="col-md-12 text-center">
                         <button class="btn btn-primary" @click="Login">登入</button>
@@ -24,20 +26,17 @@ import { ref, reactive, onMounted, onBeforeMount, onUpdated } from 'vue'
 import { type Customer } from "../type/modelType/Customer"
 import axios from 'axios'
 
-let accountValue = ref('');
-let passwordValue = ref('');
-
-const userData: Customer = {
-    ID: 0,
-    name: accountValue.value,
-    password: passwordValue.value,
-    birthday: ""
-}
+const userData = reactive({
+    id: 0,
+    name: '',
+    password: '',
+    birthday: ''
+});
 
 const Login = async () => {
 
-    userData.name = accountValue.value;
-    userData.password = passwordValue.value;
+    userData.name = userData.name;
+    userData.password = userData.password
 
     console.log(userData);
 
@@ -45,10 +44,11 @@ const Login = async () => {
     const response = await axios.post('http://localhost:8080/home/Login', userData);
     console.log(response.data.name);
     console.log("成功");
-
-    return response;
-
 }
+
+onMounted(() => {
+    console.log("onMounted");
+})
 
 </script>
 <style></style>
